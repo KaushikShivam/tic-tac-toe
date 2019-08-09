9a9a1638275940ef0cbcaabc4faed60395454627
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-10 02:20:58 +0530

    Finish game end logic

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,14 @@ class Game
   attr_accessor :matrix
   attr_accessor :players
   attr_accessor :active_player
+  attr_reader :winner
   
   def initialize
     @matrix = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
     @players = []
     @active_player = 0
     @counter=0
+    @winner = nil
   end
   def get_matrix
     result = ""
@@ -26,13 +28,27 @@ class Game
   def update_matrix(row, column)
     return false if matrix[row - 1][column - 1] != "_"
     matrix[row - 1][column - 1] = get_active_player.symbol
-    switch_active_player
+    
+    if check_move
+      @winner = @active_player
+    else
+      switch_active_player
+    end
     @counter+=1
     true
   end
+  
   def is_finished?
-    (@counter==9) 
+    if @winner
+      return 1
+    elsif @counter == 9
+      return -1
+    else
+      return 0
+    end
+  
   end
+  
   def check_move
     if (( matrix[0][0] != "_" && matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) ||
       (matrix[0][2] != "_" && matrix[0][2]== matrix[1][1] && matrix[1][1]== matrix[2][0] ))
@@ -45,7 +61,8 @@ class Game
       end
     end
     false
-
   end
-    
+  
+  
+  
 end
