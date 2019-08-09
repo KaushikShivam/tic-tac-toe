0ecbe6ef0a47fcd7c5f24a660f71b7db8bb56508
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-09 21:43:11 +0530

    Add mock matrix

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/env ruby
+
+require '../lib/user_interface'
+require '../lib/game'
+
+puts "Welcome to Tic-Tac-Toe"
+
+game = Game.new
+
+game.matrix.length.times { |n| print "#{game.matrix[n].join(' ')}\n" }
+
