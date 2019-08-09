60842ed7e515d06cc3b15838b5a19782d13fa531
abdellani <abdellani@gmail.com>
2019-08-09 17:52:01 +0100

    Add loop to read inputs from user

--- bin/main.rb
+++ bin/main.rb
@@ -2,10 +2,30 @@
 
 require '../lib/user_interface'
 require '../lib/game'
+require '../lib/player'
 
 puts "Welcome to Tic-Tac-Toe"
 
 game = Game.new
 
-game.matrix.length.times { |n| print "#{game.matrix[n].join(' ')}\n" }
+puts game.get_matrix
+puts "To play, Please provide the following inputs"
+#Validating user inputs 
+input=nil
+until input == "x" || input == "o"
+  puts "Please enter either x or o"
+  input= gets.chomp.downcase
+end
+puts input
+player_1= Player.new (input)
+player_2= Player.new ((input=="x")? "o": "x")
 
+role=true
+game_running=true
+while game_running
+  puts "(player#{(role==true)? "1": "2"}) Input a number from 1-3 to select the row"
+  row= gets.chomp.to_i
+  puts "(player#{(role==true)? "1": "2"}) Input a number from 1-3 to select the column"
+  column= gets.chomp.to_i
+  role = !role
+end
--- lib/game.rb
+++ lib/game.rb
@@ -4,5 +4,9 @@ class Game
   def initialize
     @matrix = [["_", "_", "_"], ["_", "X", "_"], ["_", "_", "_"]]
   end
-  
+  def get_matrix
+    result=""
+    matrix.length.times { |n| result += "#{matrix[n].join(' ')}\n" }
+    result
+  end
 end
