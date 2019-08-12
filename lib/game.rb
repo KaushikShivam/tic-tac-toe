00967f5d661ffe1d32bdd88668d41980708b5ebb
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-12 22:54:21 +0530

    Fix stickler errors

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@ class Game
   attr_accessor :counter
 
   def initialize
-    @matrix = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+    @matrix = [['_','_','_'], ['_','_','_'], ['_','_','_']]
     @players = []
     @active_player = 0
     @counter = 0
@@ -21,7 +21,6 @@ class Game
 
   def switch_active_player
     self.active_player = active_player.zero? ? 1 : 0
-
   end
 
   def find_active_player
@@ -29,14 +28,15 @@ class Game
   end
 
   def update_matrix(row, column)
-    if slot_taken?(row,column)
-      return false
+    if slot_taken?(row, column)
+      false
     else
       matrix[row - 1][column - 1] = find_active_player.symbol
       self.counter += 1
-      return true
+      true
     end
   end
+
   def handle_move 
     if check_move
       self.winner = active_player
@@ -44,6 +44,7 @@ class Game
       switch_active_player
     end
   end
+
   def slot_taken?(row,column)
     matrix[row - 1][column - 1] != '_'
   end
@@ -51,21 +52,21 @@ class Game
   def finished?
     winner || drawn?
   end
-  
+
   def drawn?
     counter == 9
   end
+
   def check_move
     check_diagonal || check_horizontal || check_vertical
   end
-  
+
   def check_diagonal
     if (
       matrix[0][0] != '_' &&
       matrix[0][0] == matrix[1][1] &&
       matrix[1][1] == matrix[2][2]
-    ) ||
-    (
+    ) || (
       matrix[0][2] != '_' &&
       matrix[0][2] == matrix[1][1] &&
       matrix[1][1] == matrix[2][0]
@@ -76,28 +77,25 @@ class Game
 
     false
   end
-  
+
   def check_vertical
     3.times do |x|
-      if matrix[x][0] != '_' &&
-      matrix[x][0] == matrix[x][1] &&
-      matrix[x][1] == matrix[x][2]
+      if matrix[x][0] != '_' && matrix[x][0] == matrix[x][1] &&
+         matrix[x][1] == matrix[x][2]
         return true
       end
     end
     false
   end
-  
+
   def check_horizontal
     3.times do |x|
-      if matrix[0][x] != '_' &&
-      matrix[0][x] == matrix[1][x] &&
-      matrix[1][x] == matrix[2][x]
+      if matrix[0][x] != '_' && matrix[0][x] == matrix[1][x] &&
+         matrix[1][x] == matrix[2][x]
         return true
       end
     end
     false
   end
-  
 
 end
