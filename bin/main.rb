4d885a88b1a42087cbd7db85ec1f2205e6233dde
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-12 21:59:36 +0530

    Fix logical changes with game

--- bin/main.rb
+++ bin/main.rb
@@ -5,30 +5,27 @@ require '../lib/game'
 require '../lib/player'
 
 def start_game
-  puts "Welcome to Tic-Tac-Toe"
+  puts 'Welcome to Tic-Tac-Toe'
 
   game = Game.new
 
-  puts game.get_matrix
-  puts "To play, Please provide the following inputs"
-  #Validating user inputs 
+  puts game.display_matrix
+  puts 'To play, Please provide the following inputs'
+  # Validating user inputs
   input = nil
-  until input == "x" || input == "o"
-    puts "Please enter either x or o"
+  until input == 'x' || input == 'o'
+    puts 'Please enter either x or o'
     input = gets.chomp.downcase
   end
   puts "You've selected: #{input.upcase}"
 
   # Initialise the players
   player_1 = Player.new(input)
-  player_2 = Player.new((input == "x") ? "o" : "x")
+  player_2 = Player.new(input == 'x' ? 'o' : 'x')
 
   game.players.push(player_1, player_2)
 
-  game_running = true
-  puts game.active_player
-
-  while game.is_finished? == 0
+  while game.finished? == 0
     puts "(player#{game.active_player + 1}) Input a number from 1-3 to select the row"
     row = nil
     while true
@@ -51,11 +48,11 @@ def start_game
       puts "The cell you have selected is already used!".bg_red
       puts "Please select another cell."
     end
-    puts game.get_matrix
+    puts game.display_matrix
     
-    if game.is_finished? == 1 
+    if game.finished? == 1 
       puts "Player #{game.winner + 1} Won!".bg_green
-    elsif game.is_finished? == -1
+    elsif game.finished? == -1
       puts "No one won!".bg_blue
     end    
   end
