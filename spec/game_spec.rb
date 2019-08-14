c5575eb800dcfe9c6e8572cd584ebb8679d9105b
abdellani <abdellani@gmail.com>
2019-08-14 20:54:50 +0100

    Add test from check_horizontal method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -53,5 +53,23 @@ RSpec.describe Game do
       expect(game.check_vertical).to eql(false)
     end
   end
-  
+
+  describe '#check_horizontal' do
+    it 'should return true if the slots match horizontally from first row' do
+      allow(game).to receive(:matrix).and_return([%w[x x x], %w[o _ o], %w[o _ _]])
+      expect(game.check_horizontal).to eql(true)
+    end
+    it 'should return true if the slots match horizontally from second row' do
+      allow(game).to receive(:matrix).and_return([%w[_ o o], %w[x x x], %w[_ o _]])
+      expect(game.check_horizontal).to eql(true)
+    end
+    it 'should return true if the slots match horizontally from third row' do
+      allow(game).to receive(:matrix).and_return([%w[o _ o], %w[_ _ o], %w[x x x]])
+      expect(game.check_horizontal).to eql(true)
+    end
+    it 'should return false if the slots don\'t match horizontally from any row' do
+      allow(game).to receive(:matrix).and_return([%w[x _ _], %w[x _ o], %w[_ _ o]])
+      expect(game.check_horizontal).to eql(false)
+    end
+  end
 end
